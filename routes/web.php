28295d98494ec10deba92609320e3c6499d6cb24
pankokko <teshi41@gmail.com>
2020-01-25 23:12:27 +0900

    フォルダー show設定中

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@
 Route::get("/", "TodoController@index")->name("todo/index");
 Route::get("todo/new", "TodoController@new")->name("todo/new");
 Route::post("todo/create", "TodoController@create")->name("todo/create");
+
+Route::get("folder/{id}/show","FoldersController@show")->name("folder/show");
+Route::get("folder/index","FoldersController@index")->name("folder/index");
 Route::post("folder/create", "FoldersController@create")->name("folder/create");
 
 
